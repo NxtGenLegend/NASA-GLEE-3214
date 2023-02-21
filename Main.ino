88228e8e9b2b4f660bf87857c645dfde5b2f5046
NxtGenLegend <62510055+NxtGenLegend@users.noreply.github.com>
2023-02-21 12:59:20 +0530

    Compiling Code Into Main File With Explanatory Comments; Discrepancy Found In Magnetometer OSR In FullFunctions

--- Main.ino
+++ Main.ino
@@ -1,3 +1,5 @@
+// Libraries
+
 #include <GLEE_Radio.h>
 #include "TMP117.h"
 #include "MLX90395.h"
@@ -5,6 +7,21 @@
 #include "CAP.h"
 #include "TPIS1385.h"
 #include <Wire.h>
+
+// Sensor & Radio Initializations (Debugging Mode = False: Recheck)
+TMP117 thermometer(1);
+MPU6000 accelerometer(2);
+MLX90395 magnetometer(3);
+TPIS1385 thermopile(4);
+CAP capacitive(5);
+LunaRadio Rad;
+
+// Sensor Data Variables
+float tempSample;
+sensor_float_vec_t accSample;
+mlx_sample_t magSample;
+TPsample_t thermSample;
+int capSample;
 
 // For Reference
 // Standard V4 Lunasat I2C Sensors
@@ -20,30 +37,17 @@
 // TPIS1385:  0x0D
 // ICM20602:  0x69
 
+// I2C Configurations
 byte sensorsAddrs[4] = {0x0C, 0x48, 0x0D, 0x69};
-
 String sensorNames[4] = {"Mag", "Temp", "Therm", "Accelerometer"};
 
 byte addr;      // Byte representation of I2C addresses
 byte err;       // Byte to store error responses
 int numDevices; // Accumulator for tracking number of found devices
 
-// Initiate LunaSat Radio (SX1272 Transciever)
-LunaRadio Rad;
-
-// Sensor Objects (Debugging defaults to false)
-TMP117 thermometer(1);
-MPU6000 accelerometer(2);
-MLX90395 magnetometer(3);
-TPIS1385 thermopile(4);
-CAP capacitive(5);
-
-// Sensor Data Variables
-float tempSample;
-sensor_float_vec_t accSample;
-mlx_sample_t magSample;
-TPsample_t thermSample;
-int capSample;
+// Radio Frequency Transmissions Setup
+int mode = 1; // Mentioned Below
+int id = 1;   // Only For LunaSat Testing
 
 // Individual string buffers for memory optimization
 char timeStamp_str[8] = {' '};
@@ -62,16 +66,16 @@ char cap_str[5] = {' '};
 char data_buffer[80];
 
 // Output Formatting Variables
-String printOut;       // QUESTIONABLE
+String printOut; // QUESTIONABLE
 char *sep = ","; // QUESTIONABLE
 unsigned long timeStamp;
 
-// RED Led Pin
-int LED = 4;
+// Just For Status Check
+int LED = 4; 
 
 void setup()
 {
-    // Initialization
+    //  I2C Setup
     Wire.begin();          // Begin Serial Communications
     Wire.setClock(100000); // Set Standard 100Mhz i2c speed
     Serial.begin(9600);    // Set Default
@@ -90,49 +94,6 @@ void setup()
     Wire.endTransmission();       // Send
 
     delay(50);
-
-    Serial.println("Sensor Initialization Successful\n");
-
-    pinMode(A3, OUTPUT);
-    digitalWrite(A3, HIGH);
-
-    // Start up Serial (wired) Communications
-    Serial.begin(9600);
-
-    // SENSOR INITIALIZATIONS
-
-    // Temperature Sensor - TMP117
-    // (Nothing)
-
-    // Accelerometer - MPU6000
-    accelerometer.begin();                          // Begin (check connection)
-    accelerometer.initialize();                     // Initialize (setup)
-    accelerometer.setAccelRange(MPU6000_RANGE_2_G); // Make sure acceleration range is set to 2G
-
-    // Magnetometer - MLX90393
-    magnetometer.begin_I2C();
-    // Set gain, resolution,OSR, and digital filter
-    magnetometer.setGain(8); // default value is 8, can range from 0-15
-    magnetometer.setResolution(MLX90395_RES_17, MLX90395_RES_17, MLX90395_RES_17);
-    magnetometer.setOSR(MLX90395_OSR_4);       // not default. default is MLX90395_OSR_1 and is equivalent to 0
-    magnetometer.setFilter(MLX90395_FILTER_8); // default. MLX90395_FILTER_1 is equivalent to 0
-
-    // Capacitive Sensor CAP
-    capacitive.begin(); // Begin
-
-    // Thermopile - TPIS1385
-    thermopile.begin();      // Begin
-    thermopile.readEEprom(); // Check Registers
-
-    // Initilaize Radio, turn up power (max 17) to improve range
-    Rad.initialize_radio(915.0, 7, 250.0, 12, 8);
-
-    // Set RED led pin to output
-    pinMode(LED, OUTPUT);
-}
-
-void loop()
-{
 
     byte addr;          // Current address for scanning
     int numDevices = 0; // Number of devices found
@@ -170,6 +131,43 @@ void loop()
 
     delay(1000);
 
+    // Sensor Setups (Except TMP117)
+
+    // Accelerometer - MPU6000
+    accelerometer.begin();                          // Begin (check connection)
+    accelerometer.initialize();                     // Initialize (setup)
+    accelerometer.setAccelRange(MPU6000_RANGE_2_G); // Make sure acceleration range is set to 2G
+
+    // Magnetometer - MLX90393
+    magnetometer.begin_I2C();
+    // Set gain, resolution, OSR & digital filter
+    magnetometer.setGain(8); // default value is 8, can range from 0-15
+    magnetometer.setResolution(MLX90395_RES_17, MLX90395_RES_17, MLX90395_RES_17);
+    magnetometer.setOSR(MLX90395_OSR_4);       // not default. default is MLX90395_OSR_1 and is equivalent to 0
+    magnetometer.setFilter(MLX90395_FILTER_8); // default. MLX90395_FILTER_1 is equivalent to 0
+
+    // Capacitive Sensor CAP
+    capacitive.begin(); // Begin
+
+    // Thermopile - TPIS1385
+    thermopile.begin();      // Begin
+    thermopile.readEEprom(); // Check Registers
+
+    // Initilaize Radio, turn up power (max 17) to improve range
+    Rad.initialize_radio(915.0, 7, 250.0, 12, 8);
+
+    // Set RED led pin to output
+    pinMode(LED, OUTPUT);
+
+    Serial.println("Sensor Initialization Successful\n");
+
+    pinMode(A3, OUTPUT);    // Mentioned Below
+    digitalWrite(A3, HIGH); // Just For Status Check
+}
+
+void loop()
+{
+
     // Save Timestamp
     timeStamp = millis();
 
@@ -202,7 +200,7 @@ void loop()
     // Using STRing CoPY to construction of transmission buffer with ID for transmission filtering
 
     ///////////////////////////////////////////////
-    strcpy(data_buffer, "11,"); // CHANGE TO YOUR TEAM NUMBER
+    strcpy(data_buffer, "3214,"); // Team Number
     ///////////////////////////////////////////////
 
     // Using STRing ConCaT to append indiviual character arrays (psudo strings) to
