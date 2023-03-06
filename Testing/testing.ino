2141a20153b3d6798ee88be4226f85e19b0ccf5c
nicesharkk <125489950+nicesharkk@users.noreply.github.com>
2023-03-06 21:47:25 +0530

    kalman filter

--- Testing/testing.ino
+++ Testing/testing.ino
@@ -1,3 +1,18 @@
+
+
+
+
+#include <CAP.h>
+#include <GLEE_Radio.h>
+#include <GLEE_Sensor.h>
+#include <LunaSat.h>
+#include <MLX90393.h>
+#include <MLX90395.h>
+#include <MPU6000.h>
+#include <TMP117.h>
+#include <TPIS1385.h>
+
+#include <SimpleKalmanFilter.h>
 #include <GLEE_Radio.h>
 #include "TMP117.h"
 #include "MLX90395.h"
