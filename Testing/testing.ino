040a7d28c9395484279d2b77815f4f7f6590684a
nicesharkk <125489950+nicesharkk@users.noreply.github.com>
2023-03-07 19:38:06 +0530

    kf update

--- Testing/testing.ino
+++ Testing/testing.ino
@@ -1,25 +1,12 @@
+#include <cmath>
+#include <eigen.h>
+#include <ArduinoEigen.h>
+#include <ArduinoEigenDense.h>
+#include <ArduinoEigenSparse.h>
 
 
 
 
-#include <CAP.h>
-#include <GLEE_Radio.h>
-#include <GLEE_Sensor.h>
-#include <LunaSat.h>
-#include <MLX90393.h>
-#include <MLX90395.h>
-#include <MPU6000.h>
-#include <TMP117.h>
-#include <TPIS1385.h>
-
-#include <SimpleKalmanFilter.h>
-#include <GLEE_Radio.h>
-#include "TMP117.h"
-#include "MLX90395.h"
-#include "MPU6000.h"
-#include "CAP.h"
-#include "TPIS1385.h"
-#include <Wire.h>
 
 void setup(){
 
