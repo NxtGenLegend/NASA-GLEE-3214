293c84e8c96af077d94f937bc641c45fb173eae4
nicesharkk <125489950+nicesharkk@users.noreply.github.com>
2023-03-11 22:28:26 +0530

    Bhumi Accelerometer calibration

--- Testing/Accelerometer_Calibration.ino
+++ Testing/Accelerometer_Calibration.ino
@@ -1,8 +1,6 @@
 #include "MPU6000.h"
-#include "Wire.h"
 
 MPU6000 accelerometer(1, false);
-<<<<<<< Updated upstream
 const int calibrationSize = 40;
 sensor_float_vec_t calibrationPoints[calibrationSize];
 float correctedX[calibrationSize];
@@ -10,66 +8,6 @@ float correctedY[calibrationSize];
 float correctedZ[calibrationSize];
 float baseX, baseY, baseZ;
 float modeX, modeY, modeZ;
-=======
- float modeX = 0.0;
- float modeY = 0.0;
- float modeZ = 0.0;
- float baseX = 0.0;
- float baseY = 0.0;
- float baseZ = 0.0;
- float correctedX = 0.0;
- float correctedY = 0.0;
- float correctedZ = 0.0;
- sensor_float_vec_t acc;
-float xAngle = 0.0;
-float yAngle = 0.0;
-float zAngle = 0.0;
-float avgX = 0.0;
-float avgY = 0.0;
-float avgZ = 0.0;
-
-float calibrationSize = 40;
-float count;
-float yzAcceleration;
-float xzAcceleration;
-float xyAcceleration;
-
-
-//convert from C++ to arduino
-sensor_float_vec_t MPU6000::getMPSAccel(sensor_float_vec_t GAccel){
-	sensor_float_vec_t accelMPS;
-	accelMPS.x = GAccel.x * MPU_ONE_G;
-	accelMPS.y = GAccel.y * MPU_ONE_G;
-	accelMPS.z = GAccel.z * MPU_ONE_G;
-	//return accelMPS;    
-
-
- float modeX (float array[], int n);
- sort(arr, arr + n); //sort is not a thing??? define n
-   float max_count = 1, res = arr[0], count = 1; 
-   
-   for (float i = 1; i < n; i++) 
-        if (arr[i] == arr[i - 1]) 
-            count++; 
-        else { 
-            if (count > max_count) 
-                max_count = count; 
-                res = arr[i - 1]; 
-            
-            count = 1; 
-        } 
-        
-     if (count > max_count) 
-    { 
-        max_count = countX; 
-        res = arr[n - 1]; 
-    } 
-    
-    return res;
-}
-
-
->>>>>>> Stashed changes
 
 void setup() {
   Serial.begin(9600);
