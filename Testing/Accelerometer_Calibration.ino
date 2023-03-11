7c2337d15dfadc9ce99233c4f51462ffb61443b0
bhumi <98217028+spaceoreo@users.noreply.github.com>
2023-03-11 21:24:44 +0530

    Update Accelerometer_Calibration.ino
    accelerometer zero calibration added

--- Testing/Accelerometer_Calibration.ino
+++ Testing/Accelerometer_Calibration.ino
@@ -1,190 +1,78 @@
-/* take(find) first couple readings and mean then change the set's values according to error and store both values.
-    mode after all values are processed
-*/ 
-
 #include "MPU6000.h"
 #include "Wire.h"
 
-static boolean firstX = true;
-static boolean firstY = true;
-static boolean firstZ = true;
 MPU6000 accelerometer(1, false);
- float modeX = 0.0;
- float modeY = 0.0;
- float modeZ = 0.0;
- float baseX = 0.0;
- float baseY = 0.0;
- float baseZ = 0.0;
- float correctedX = 0.0;
- float correctedY = 0.0;
- float correctedZ = 0.0;
- sensor_float_vec_t acc;
-float xAngle = 0.0;
-float yAngle = 0.0;
-float zAngle = 0.0;
-float avgX = 0.0;
-float avgY = 0.0;
-float avgZ = 0.0;
-
-float calibrationSize = 40;
-float count;
-float yzAcceleration;
-float xzAcceleration;
-float xyAcceleration;
-
-
-//convert from C++ to arduino
-sensor_float_vec_t MPU6000::getMPSAccel(sensor_float_vec_t GAccel){
-	sensor_float_vec_t accelMPS;
-	accelMPS.x = GAccel.x * MPU_ONE_G;
-	accelMPS.y = GAccel.y * MPU_ONE_G;
-	accelMPS.z = GAccel.z * MPU_ONE_G;
-	//return accelMPS;    
-
-
- float modeX ( float array[], int n);
- sort(arr, arr + n); //sort is not a thing??? define n
-   float max_count = 1, res = arr[0], count = 1; 
-   
-   for (float i = 1; i < n; i++) 
-        if (arr[i] == arr[i - 1]) 
-            count++; 
-        else { 
-            if (count > max_count) 
-                max_count = count; 
-                res = arr[i - 1]; 
-            
-            count = 1; 
-        } 
-        
-     if (count > max_count) 
-    { 
-        max_count = countX; 
-        res = arr[n - 1]; 
-    } 
-    
-    return res;
-}
-
-
+const int calibrationSize = 40;
+sensor_float_vec_t calibrationPoints[calibrationSize];
+float correctedX[calibrationSize];
+float correctedY[calibrationSize];
+float correctedZ[calibrationSize];
+float baseX, baseY, baseZ;
+float modeX, modeY, modeZ;
 
 void setup() {
   Serial.begin(9600);
   accelerometer.begin();
   accelerometer.initialize();
   accelerometer.setAccelRange (MPU6000_RANGE_2_G);
-  sensor_float_vec_t calibrationPoints[calibrationSize];  // Array of vectors for storing calibration points  
-    for(int i = 0; i < calibrationSize; i++){
-        calibrationPoints[i] = accelerometer.getSample();
-    }
-
-for(int j = 0; j < calibrationSize; j++){
-        avgX = avgX + calibrationPoints[j].x;
-        avgY = avgY + calibrationPoints[j].y;
-        
-    }
-    avgX = avgX/calibrationSize;
-    avgY = avgY/calibrationSize;
-    yzAcceleration = sqrt(pow(accMPS.y,2) + pow(accMPS.z,2)); 
-    xzAcceleration = sqrt(pow(accMPS.x,2) + pow(accMPS.z,2)); 
-    xyAcceleration = sqrt(pow(accMPS.x,2) + pow(accMPS.y,2)); 
-
+  
+  
+  float sumX = 0.0, sumY = 0.0, sumZ = 0.0;
+  for (int i = 0; i < calibrationSize; i++) {
+    calibrationPoints[i] = accelerometer.getSample();
+    sumX += calibrationPoints[i].x;
+    sumY += calibrationPoints[i].y;
+    sumZ += calibrationPoints[i].z;
+  }
+  baseX = sumX / calibrationSize;
+  baseY = sumY / calibrationSize;
+  baseZ = sumZ / calibrationSize;
+  
+  
+  for (int i = 0; i < calibrationSize; i++) {
+    correctedX[i] = calibrationPoints[i].x - baseX;
+    correctedY[i] = calibrationPoints[i].y - baseY;
+    correctedZ[i] = calibrationPoints[i].z - baseZ;
+  }
 }
 
 void loop() {
-  acc= accelerometer.getSample();
-  accelerometer.getEvent(&event);
-
-  float scaleX = event.acceleration.X - baseX;
-  float scaleY = event.acceleration.Y - baseY;
-  float scaleZ = event.acceleration.Z - baseZ;
   
-  Serial.print("X : "); Serial.print(scaleX);
-  Serial.print("Y : "); Serial.print(scaleY);
-  Serial.print("Z : "); Serial.print(scaleZ);
-
-  xAngle = accMPS.x/ yzAcceleration;
-    xAngle = atan(xAngle)*(180/3.141);
-    Serial.print("X Angle (Roll): ");
-    Serial.print(xAngle);
-    Serial.println("º");
-  yAngle = accMPS.y/xzAcceleration;
-    yAngle = atan(yAngle)*(180/3.141);
-    Serial.print("Y Angle (Pitch): ");
-    Serial.print(yAngle);
-    Serial.println("º");
-  zAngle = accMPS.z/xyAcceleration; //refer to line 35 issue
-    zAngle = (atan(zAngle)*(180/3.141));
-    Serial.print("Z Angle (Yaw): ");
-    Serial.print(zAngle);
-    Serial.println("º");
-    Serial.println();
-  delay(500);
+  sensor_float_vec_t sample = accelerometer.getSample();
   
 
-    float baseX() ;
-    float n;
-    float arr[50];
-    float count; "Enter the size of array: ";
-    
-  Serial.print("ModeX"); Serial.print(correctedX);
+  float cx = sample.x - baseX;
+  float cy = sample.y - baseY;
+  float cz = sample.z - baseZ;
   
-  float modeY( float array[], int n) ;
-    sort(arr, arr + n); //arr vs array 
-   float max_count = 1, res = arr[0], count = 1; 
-   
-   for (float i = 1; i < n; i++) { 
-        if (arr[i] == arr[i - 1]) 
-            count++; 
-        else { 
-            if (count > max_count) { 
-                max_count = count; 
-                res = arr[i - 1]; 
-            } 
-            count = 1; 
-        } 
-        
-     if (count > max_count) 
-    { 
-        max_count = countY; 
-        res = arr[n - 1]; 
-    } 
-    
-    return res;
-}
-    float baseY();
-    float n;
-    float arr[50];
-    count<<"Enter the size of array: ";
-  Serial.print("ModeY"); Serial.print(correctedY);
+
+  correctedX[calibrationSize] = cx;
+  correctedY[calibrationSize] = cy;
+  correctedZ[calibrationSize] = cz;
   
-  float modeZ( float array[], int n);
-    sort(array, array + n);
-   float max_count = 1, res = array[0], count = 1; 
-   
-   for (float i = 1; i < n; i++) { 
-        if (arr[i] == arr[i - 1]) 
-            count++; 
-        else { 
-            if (count > max_count) { 
-                max_count = count; 
-                res = arr[i - 1]; 
-            } 
-            count = 1; 
-        } 
-        
-     if (count > max_count) 
-    { 
-        max_count = countZ; 
-        res = arr[n - 1]; 
-    } 
-    
-    return res;
-}
-    float baseZ();
-    float n;
-    float arr[50];
-    count<<"Enter the size of array: ";
-  Serial.print("ModeZ"); Serial.print(correctedZ);//correctedZ 
+
+  modeX = calculateMode(correctedX, calibrationSize);
+  modeY = calculateMode(correctedY, calibrationSize);
+  modeZ = calculateMode(correctedZ, calibrationSize);
   
+  
+  Serial.print("Corrected X: "); Serial.print(cx);
+  Serial.print("  Base X: "); Serial.print(baseX);
+  Serial.print("  Mode X: "); Serial.print(modeX);
+  Serial.println();
+  
+  Serial.print("Corrected Y: "); Serial.print(cy);
+  Serial.print("  Base Y: "); Serial.print(baseY);
+  Serial.print("  Mode Y: "); Serial.print(modeY);
+  Serial.println();
+  
+  Serial.print("Corrected Z: "); Serial.print(cz);
+  Serial.print("  Base Z: "); Serial.print(baseZ);
+  Serial.print("  Mode Z: "); Serial.print(modeZ);
+  Serial.println();
 }
+
+
+float calculateMode(float arr[], int size) {
+  float modeValue = 0.0;
+  int maxCount = 0
